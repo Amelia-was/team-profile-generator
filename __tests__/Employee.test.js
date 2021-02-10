a8e9798427d2cfabcd5a2a32217744ac17667514
Deisel <74831846+Amelia-was@users.noreply.github.com>
2021-02-10 18:30:52 -0500

    create employee obj

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -2,7 +2,9 @@ const { expect } = require('@jest/globals');
 const Employee = require('../lib/Employee');
 
 test('creates employee object', () => {
-    const employee = new Employee('Anne');
+    const employee = new Employee('Anne', 123, 'email@internet.com');
 
     expect(employee.name).toBe('Anne');
+    expect(employee.id).toBeGreaterThanOrEqual(1);
+    expect(employee.name).toEqual(expect.any(String));
 })
