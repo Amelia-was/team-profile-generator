b13018032c373149f2a1ffced277618551fcba99
Deisel <74831846+Amelia-was@users.noreply.github.com>
2021-02-11 13:45:17 -0500

    destructure employees array and write to html file

--- src/template.js
+++ src/template.js
@@ -1,7 +1,10 @@
+const Manager = require('../lib/Manager.js');
+const Engineer = require('../lib/Engineer.js');
+const Intern = require('../lib/Intern.js');
 const generatePage = require('./generate-page.js');
 
-const pageTemplate = (employees) => {
-    console.log(employees);
+const pageTemplate = employeesArr => {
+    console.log(employeesArr);
 
     return `<!DOCTYPE html>
 <html lang="en">
@@ -13,7 +16,67 @@ const pageTemplate = (employees) => {
 <body>
 
 <h1>My Team</h1>
-${employees}
+
+${employeesArr
+            .filter(employee => employee instanceof Manager)
+            .map(({ name, id, email, officeNumber }) => {
+                return `
+          <div>
+            <h3>${name}</h3>
+            <h5>
+              ID: ${id}
+            </h5>
+            <h5>
+              Email: ${email}
+            </h5>
+            <h5>
+              Office Number: ${officeNumber}
+            </h5>
+          </div>
+        `;
+            })
+            .join('')}
+
+${employeesArr
+            .filter(employee => employee instanceof Engineer)
+            .map(({ name, id, email, github }) => {
+                return `
+          <div>
+            <h3>${name}</h3>
+            <h5>
+              ID: ${id}
+            </h5>
+            <h5>
+              Email: ${email}
+            </h5>
+            <h5>
+              GitHub: <a href='https://github.com/${github}'>${github}</a>
+            </h5>
+          </div>
+        `;
+            })
+            .join('')}
+
+${employeesArr
+            .filter(employee => employee instanceof Intern)
+            .map(({ name, id, email, school }) => {
+                return `
+          <div>
+            <h3>${name}</h3>
+            <h5>
+              ID: ${id}
+            </h5>
+            <h5>
+              Email: ${email}
+            </h5>
+            <h5>
+              School: ${school}
+            </h5>
+          </div>
+        `;
+            })
+            .join('')}
+
 </body>
 </html>`
 }
