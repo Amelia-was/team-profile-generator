4540814e029c42238cc541d74db436b36fd1dc09
Deisel <74831846+Amelia-was@users.noreply.github.com>
2021-02-11 11:39:36 -0500

    accept user input for engineer and intern

--- index.js
+++ index.js
@@ -9,30 +9,32 @@ const employees = [];
 // prompt user for team members
 const promptManager = () => {
     console.log(`
-================
+╔══════════════╗
+
 Create your team
-================
+
+╚══════════════╝
 `);
     return inquirer
         .prompt([{
             type: 'text',
             name: 'name',
-            message: 'Please enter the manager name'
+            message: 'Please enter your manager\'s name'
         },
         {
             type: 'text',
             name: 'id',
-            message: 'Please enter the manager ID'
+            message: 'Please enter your manager\'s ID'
         },
         {
             type: 'text',
             name: 'email',
-            message: 'Please enter the manager email'
+            message: 'Please enter your manager\'s email'
         },
         {
             type: 'text',
             name: 'officeNumber',
-            message: 'Please enter the manager office number'
+            message: 'Please enter your manager\'s office number'
         },
         {
             type: 'confirm',
@@ -42,7 +44,95 @@ Create your team
         }])
         // destructure manager properties from the prompt object
         .then(({ name, id, email, officeNumber, confirmAddEmployee }) => {
-            const employee = new Manager(name, id, email, officeNumber);
+            let employee = new Manager(name, id, email, officeNumber);
+            employees.push(employee);
+            //console.log(employees);
+            if (confirmAddEmployee) {
+                // console.log(employees);
+                // console.log('Let\'s add another employee!');
+                promptEngineer();
+            }
+            else {
+                console.log(employees);
+                console.log('No more employees!');
+            }
+        })
+}
+
+const promptEngineer = () => {
+    return inquirer
+        .prompt([{
+            type: 'text',
+            name: 'name',
+            message: 'Please enter your engineer\'s name'
+        },
+        {
+            type: 'text',
+            name: 'id',
+            message: 'Please enter your engineer\'s ID'
+        },
+        {
+            type: 'text',
+            name: 'email',
+            message: 'Please enter your engineer\'s email'
+        },
+        {
+            type: 'text',
+            name: 'github',
+            message: 'Please enter your engineer\'s GitHub username'
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAddEmployee',
+            message: 'Would you like to enter another employee?',
+            default: false
+        }])
+        .then(({ name, id, email, github, confirmAddEmployee }) => {
+            let employee = new Engineer(name, id, email, github);
+            employees.push(employee);
+            //console.log(employees);
+            if (confirmAddEmployee) {
+                // console.log(employees);
+                // console.log('Let\'s add another employee!');
+                promptIntern();
+            }
+            else {
+                console.log(employees);
+                console.log('No more employees!');
+            }
+        })
+}
+
+const promptIntern = () => {
+    return inquirer
+        .prompt([{
+            type: 'text',
+            name: 'name',
+            message: 'Please enter your intern\'s name'
+        },
+        {
+            type: 'text',
+            name: 'id',
+            message: 'Please enter your intern\'s ID'
+        },
+        {
+            type: 'text',
+            name: 'email',
+            message: 'Please enter your intern\'s email'
+        },
+        {
+            type: 'text',
+            name: 'school',
+            message: 'Please enter your intern\'s school'
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAddEmployee',
+            message: 'Would you like to enter another employee?',
+            default: false
+        }])
+        .then(({ name, id, email, school, confirmAddEmployee }) => {
+            let employee = new Intern(name, id, email, school);
             employees.push(employee);
             //console.log(employees);
             if (confirmAddEmployee) {
